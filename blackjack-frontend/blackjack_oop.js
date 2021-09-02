cd462cd783c43be8087a1c081a7cd50f931833c4
kashcoding <88490912+kashcoding@users.noreply.github.com>
2021-09-02 16:41:22 +0100

    Two new cards load when page opens

--- blackjack-frontend/blackjack_oop.js
+++ blackjack-frontend/blackjack_oop.js
@@ -1,146 +1,144 @@
 function shuffle(array, seed = 1) {
-  let currentIndex = array.length
-  let temporaryValue, randomIndex
+  let currentIndex = array.length;
+  let temporaryValue, randomIndex;
 
   let random = () => {
-    var x = Math.sin(seed++) * 10000
-    return x - Math.floor(x)
-  }
+    var x = Math.sin(seed++) * 10000;
+    return x - Math.floor(x);
+  };
 
   while (0 !== currentIndex) {
-    randomIndex = Math.floor(random() * currentIndex)
-    currentIndex -= 1
-    temporaryValue = array[currentIndex]
-    array[currentIndex] = array[randomIndex]
-    array[randomIndex] = temporaryValue
+    randomIndex = Math.floor(random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
-  return array
+  return array;
 }
 
 class Card {
   constructor(rank, suit) {
-    this.rank = rank
-    this.suit = suit
+    this.rank = rank;
+    this.suit = suit;
   }
 
   toString() {
-    return `${this.rank}${this.suit}`
+    return `${this.rank}${this.suit}`;
   }
 
   get points() {
-    let points = 0
-    if (this.rank === 'A') {
-      points = 11
+    let points = 0;
+    if (this.rank === "A") {
+      points = 11;
     } else if (!parseInt(this.rank) || this.rank == 10) {
-      points = 10
+      points = 10;
     } else {
-      points = this.rank
+      points = this.rank;
     }
-    return parseInt(points)
+    return parseInt(points);
   }
 }
 
 class Hand {
   constructor(deck) {
-    this.deck = deck
-    this.cardsInHand = []
-    this.drawCard()
-    this.drawCard()
+    this.deck = deck;
+    this.cardsInHand = [];
+    this.drawCard();
+    this.drawCard();
     if (!this.cardsInHand.every((card) => card instanceof Card)) {
-      throw new TypeError('A Hand can only contain Cards')
+      throw new TypeError("A Hand can only contain Cards");
     }
   }
 
   get points() {
-    let totalPointsFromHand = 0
+    let totalPointsFromHand = 0;
     if (this.cardsInHand.length && this.cardsInHand.length >= 2) {
       if (
         this.cardsInHand[0].points == 11 &&
         this.cardsInHand[1].points == 11
       ) {
-        totalPointsFromHand = 21
+        totalPointsFromHand = 21;
       } else {
         totalPointsFromHand = this.cardsInHand.reduce((accumulator, card) => {
-          return accumulator + card.points
-        }, 0)
+          return accumulator + card.points;
+        }, 0);
       }
       if (this.cardsInHand.length > 5 && totalPointsFromHand < 21) {
-        totalPointsFromHand = 21
+        totalPointsFromHand = 21;
       }
     }
-    return totalPointsFromHand
+    return totalPointsFromHand;
   }
 
   drawCard() {
-    this.cardsInHand.push(this.deck.draw())
+    this.cardsInHand.push(this.deck.draw());
   }
 
   get latestCard() {
-    return this.cardsInHand[this.cardsInHand.length - 1]
+    return this.cardsInHand[this.cardsInHand.length - 1];
   }
 
   toString(player) {
-    return `${player} hand is ${this.cardsInHand}\n(${this.points})`
+    return `${player} hand is ${this.cardsInHand}\n(${this.points})`;
   }
 }
 
 class Deck {
   constructor() {
-    this.cards = this.createDeck()
+    this.cards = this.createDeck();
   }
 
   createDeck() {
-    let deck = []
-    const suit = ['S', 'D', 'C', 'H']
-    const rank = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+    let deck = [];
+    const suit = ["S", "D", "C", "H"];
+    const rank = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
     for (let i = 0; i < suit.length; i++) {
       for (let j = 0; j < rank.length; j++) {
-        deck.push(new Card(rank[j], suit[i]))
+        deck.push(new Card(rank[j], suit[i]));
       }
     }
-    return deck
+    return deck;
   }
 
   getCards() {
-    return this.cards
+    return this.cards;
   }
 
   draw() {
-    return this.cards.shift()
+    return this.cards.shift();
   }
 
   shuffle(seed = Date.now()) {
-    return shuffle(this.cards, seed)
+    return shuffle(this.cards, seed);
   }
 }
 
-window.addEventListener('DOMContentLoaded', function () {
-  const deck = new Deck()
-  deck.shuffle()
-  const card1 = deck.draw()
-  const card2 = deck.draw()
-
-  alert(`Your random card is ${card.toString()}`)
-})
-
-document.getElementById('btn').addEventListener('click', function () {
-  const deck = new Deck()
-  deck.shuffle()
-  const card = deck.draw()
-
-  let ul = document.getElementById('random-card')
-  let li = document.createElement('li')
-  li.className = 'card'
-
-  li.innerText = `${card.toString()} (${card.points} points)`
-
-  if (document.querySelectorAll('.card').length >= 5) {
-    ul.removeChild(document.querySelectorAll('.card')[0])
+function setSrc(card) {
+  let cardFace;
+  if (card.toString()[1] === "0") {
+    cardFace = `0${card.suit}`;
+  } else {
+    cardFace = card.toString();
   }
 
-  if (card.rank === 'A') {
-    li.classList.add('ace')
-  }
+  return `https://deckofcardsapi.com/static/img/${cardFace}.png`;
+}
 
-  ul.appendChild(li)
-})
+window.addEventListener("DOMContentLoaded", function () {
+  const deck = new Deck();
+  deck.shuffle();
+  const card1 = deck.draw();
+  const card2 = deck.draw();
+
+  let card1img = document.createElement("img");
+  let card2img = document.createElement("img");
+
+  card1img.setAttribute("id", "card1");
+  card2img.setAttribute("id", "card2");
+
+  card1img.src = setSrc(card1);
+  card2img.src = setSrc(card2);
+
+  document.querySelector(".cards").append(card1img, card2img);
+});
