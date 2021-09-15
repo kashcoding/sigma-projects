724ceddffdb7d71cb1a50dc25de3429813c90f03
kashcoding <88490912+kashcoding@users.noreply.github.com>
2021-09-15 11:33:08 +0100

    Added more country information

--- countries/src/CountryDisplay.js
+++ countries/src/CountryDisplay.js
@@ -15,7 +15,7 @@ class CountryDisplay extends React.Component {
   async componentDidMount() {
     // console.log(this.state.countryCode);
     const apiResponse = await fetch(
-      `https://restcountries.eu/rest/v2/alpha/${this.state.countryCode}?fields=name;capital;population;flag`
+      `https://restcountries.eu/rest/v2/alpha/${this.state.countryCode}?fields=name;capital;population;flag;languages;currencies;subregion;timezones`
     );
     const countryData = await apiResponse.json();
 
@@ -28,12 +28,21 @@ class CountryDisplay extends React.Component {
     } else {
       return (
         <section className="main">
+          <h4>{this.state.countryData.subregion}</h4>
           <h1>{this.state.countryData.name}</h1>
-          <img src="URL" alt="Flag"></img>
+          <img
+            className="flag"
+            src={this.state.countryData.flag}
+            alt="Flag"
+          ></img>
           <h3>Capital: {`${this.state.countryData.capital}`}</h3>
-          <h3>Population: {`${this.state.countryData.population}`}</h3>
-          <h3>Language</h3>
-          <h3>Currency</h3>
+          <h3>
+            Population:{" "}
+            {`${this.state.countryData.population.toLocaleString()}`}
+          </h3>
+          <h3>Language: {`${this.state.countryData.languages[0].name}`}</h3>
+          <h3>Currency: {`${this.state.countryData.currencies[0].name}`}</h3>
+          <h3>Timezone: {this.state.countryData.timezones}</h3>
         </section>
       );
     }
